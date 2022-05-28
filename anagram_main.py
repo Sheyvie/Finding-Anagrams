f10a17c2c60dc79371d07e2a617f643c982ef05f
Yvonne <84870035+Sheyvie@users.noreply.github.com>
2022-05-28 16:52:51 +0300

    Add files via upload

---
+++ anagram_main.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    word = input('Enter a word: ')
+    anagram = input('Enter second word: ')
+
+    sorted_str1 = sorted(word)
+    sorted_str2 = sorted(anagram)
+
+    if (sorted_str1 == sorted_str2):
+     print (True)
+    else:
+     
+     return False
+find_anagram("word","anagram")
